305c6a93d03e0c29d97085bba4b82f418faff616
X474X <133019572+X474X@users.noreply.github.com>
2023-09-05 15:43:57 +0300

    Finished base gameplay

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let chari = document.querySelector(".char");
 let bulbasaur = document.querySelector(".bulb");
 let fight = document.querySelector(".fight");
-
+let name = document.querySelector(".name");
+let health = document.querySelector(".health");
+let cD = document.querySelector(".char-damage");
+let bD = document.querySelector(".bul-damage ");
 let currentPlayer = "bulb";
 
+let charHealth = 100;
+let bulbHealth = 100;
+
+//Damage function
 const damage = (character, current) => {
   character.style.filter = "saturate(10)";
   if (current === "dragon") {
@@ -23,12 +30,89 @@ const damage = (character, current) => {
   }, 500);
 };
 
+//Battle function
+const battle = (currentPlayer) => {
+  const randomNumber = 10 + Math.floor(Math.random() * 20);
+
+  if (currentPlayer === "char") {
+    bD.innerHTML = "-" + randomNumber;
+    bD.style.visibility = "visible";
+
+    setTimeout(() => {
+      bD.style.visibility = "hidden";
+    }, 700);
+
+    setTimeout(() => {
+      health.innerHTML = bulbHealth;
+      health.innerHTML = Number(health.innerHTML) - randomNumber;
+      if (Number(health.innerHTML) <= 0) {
+        winner();
+        health.innerHTML = "0";
+      }
+      bulbHealth = bulbHealth - randomNumber;
+    }, 1100);
+
+    setTimeout(() => {
+      name.innerHTML = "Bulbasor";
+    }, 1000);
+  } else {
+    cD.innerHTML = "-" + randomNumber;
+    cD.style.visibility = "visible";
+
+    setTimeout(() => {
+      cD.style.visibility = "hidden";
+    }, 700);
+
+    setTimeout(() => {
+      health.innerHTML = charHealth;
+      health.innerHTML = Number(health.innerHTML) - randomNumber;
+      charHealth = charHealth - randomNumber;
+      if (Number(health.innerHTML) <= 0) {
+        winner();
+        health.innerHTML = "0";
+      }
+    }, 1100);
+
+    setTimeout(() => {
+      name.innerHTML = "Charizard";
+    }, 1000);
+  }
+};
+
+const winner = () => {
+  if (charHealth <= 0) {
+    chari.style.filter = "saturate(0)";
+    chari.style.transform = "rotateX(-80deg)";
+    winnerAnimation(bulbasaur);
+  } else {
+    bulbasaur.style.filter = "saturate(0)";
+    bulbasaur.style.transform = "rotateX(-80deg)";
+    winnerAnimation(chari);
+  }
+};
+
+const winnerAnimation = (character) => {
+  character.style.position = "absolute";
+  character.style.top = "-100px";
+};
+
 fight.addEventListener("click", () => {
   if (currentPlayer === "bulb") {
-    damage(chari, "dragon");
-    currentPlayer = "char";
+    if (charHealth > 0 && bulbHealth > 0) {
+      damage(chari, "dragon");
+      battle("bulb");
+      currentPlayer = "char";
+    }
   } else {
-    damage(bulbasaur, "broasca");
-    currentPlayer = "bulb";
+    if (charHealth > 0 && bulbHealth > 0) {
+      damage(bulbasaur, "broasca");
+      currentPlayer = "bulb";
+      battle("char");
+    }
   }
 });
+
+//1) Sa creem o functie care sa modifice numele si HP curent al pokemonilor.
+//2) Sa creem o functie care sa dea damage pokemonilor.
+//3) Functie Ultimata
+//4) Conditie de win/lose
